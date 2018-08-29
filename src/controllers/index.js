cbb36a913a6406f323d390bbd660d52d0494d938
lubnaabad <lubna_mosa@hotmail.com>
2018-08-29 12:14:13 +0300

    skelton

---
+++ src/controllers/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const router = express.Router();
+const error = require('./error');
+
+router.use(error.client);
+router.use(error.server);
+
+module.exports = router;
